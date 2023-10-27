a91c60616630b9a45a3938c7e495b0bd7e15e5f8
alexindevs <alexindevs@gmail.com>
2023-10-28 00:06:16 +0100

    Feat: Add Url Shortener Service

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(express.json());
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -18,6 +20,37 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+let urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+function validator(value) {
+  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+}
+
+
+app.post('/api/shorturl', (req, res) => {
+  const longUrl = req.body.url;
+  const shortUrl = Math.random().toString(36).slice(2);
+  if (validator(longUrl)) {
+    urlDatabase[shortUrl] = longUrl;
+    res.json({ "original_url": longUrl, "short_url": shortUrl });
+  }
+  else {
+    res.json({error: "Invalid URL"});
+  }
+});
+
+app.get("/api/shorturl/:shortUrl", (req, res) => {
+  const { shortUrl } = req.params;
+  if (Object.prototype.hasOwnProperty.call(urlDatabase, shortUrl)) {
+    res.redirect(urlDatabase[shortUrl]);
+} else {
+  res.json({error: "Invalid URL"});
+}
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
